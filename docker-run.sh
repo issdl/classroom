6107a2577ac225f9dc5d5dd8a20d9305037b6938
Maciej Szankin <maciej@szankin.pl>
2018-05-28 22:36:41 -0700

    Added preserving user files between docker sessions

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 
-docker run -p 8000:8000 -v $(pwd):/opt/core/:ro -v /opt/data:/opt/data:ro -v /opt/classes:/opt/classes:ro -ti issdl/classroom /opt/core/startup.sh
+docker run -p 8000:8000 -v /opt/students/:/home/:rw -v $(pwd):/opt/core/:ro -v /opt/data:/opt/data:ro -v /opt/classes:/opt/classes:ro -ti issdl/classroom /opt/core/startup.sh
--- startup.sh
+++ startup.sh
@@ -5,8 +5,9 @@ for N in `seq -f %02g 1 $NUMBER_OF_USERS`;
 do 
   echo Copying class files for user training$N;
   #cp -R /opt/classes/* /home/training$N/;
-  rsync -a --ignore-existing /opt/classes/* /home/training$N/;
+  rsync -a --ignore-existing --exclude=data /opt/classes/* /home/training$N/;
   chown -R training$N:training$N /home/training$N;
+  ln -s /opt/data/ /home/training$n/data;
 done;
 
 jupyterhub

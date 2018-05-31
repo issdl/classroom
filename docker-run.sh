94bc69e262f1d54dc449b9bd4a9f22e0ac73103e
Maciej Szankin <maciej@szankin.pl>
2018-05-30 21:20:36 -0700

    Changed volumes to be mounted in /opt/issdl/ instead of /opt/ #2

--- docker-run.sh
+++ docker-run.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /opt/issdl/students/ /opt/issdl/core/ /opt/issdl/data/ /opt/issdl/classes/
 
-docker run -p 8000:8000 -v /opt/issdl/students/:/home/:rw -v $(pwd):/opt/issdl/core/:ro -v /opt/issdl/data:/opt/data:ro -v /opt/issdl/classes:/opt/classes:ro -ti issdl/classroom /opt/issdl/core/startup.sh
+docker run -p 8000:8000 -v /opt/issdl/students/:/home/:rw -v $(pwd):/opt/issdl/core/:ro -v /opt/issdl/data:/opt/issdl/data:ro -v /opt/issdl/classes:/opt/issdl/classes:ro -ti issdl/classroom /opt/issdl/core/startup.sh

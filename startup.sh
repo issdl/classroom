20b961595f744ab2286f4a78cdb9a74ebe0c4bae
Maciej Szankin <maciej@szankin.pl>
2018-05-30 21:22:31 -0700

    Changed volumes to be mounted in /opt/issdl/ instead of /opt/ #3

--- startup.sh
+++ startup.sh
@@ -4,10 +4,9 @@ NUMBER_OF_USERS=10;
 for N in `seq -f %02g 1 $NUMBER_OF_USERS`; 
 do 
   echo Copying class files for user training$N;
-  #cp -R /opt/classes/* /home/training$N/;
-  rsync -a --ignore-existing --exclude=data /opt/classes/* /home/training$N/;
+  rsync -a --ignore-existing --exclude=data /opt/issdl/classes/* /home/training$N/;
   chown -R training$N:training$N /home/training$N;
-  ln -s /opt/data/ /home/training$N/data;
+  ln -s /opt/issdl/data/ /home/training$N/data;
 done;
 
 jupyterhub

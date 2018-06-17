2ea17adc0a791bc1eaa620a8469db7ce4d721139
Maciej Szankin <maciej@szankin.pl>
2018-06-17 07:31:24 +0200

    Added TF built from sources

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
-docker build -t issdl/classroom docker/
+
+mkdir -p docker/packages/
+wget -c https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh -O docker/packages/anaconda.sh
+sudo docker build -t issdl/classroom docker/
 
 # Update environment.yml file to match current version
 ./utils/docker-get-env.sh

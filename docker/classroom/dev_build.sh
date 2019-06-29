93e6fd18f0de03d9c408ee1088edc45d2e9bbefa
Maciej Szankin <maciej@szankin.pl>
2019-06-29 14:46:19 +0200

    Added scripts for running development deployments of the classroom

---
+++ docker/classroom/dev_build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+time docker build -t issdl/classroom:2019-dev .
+
---
+++ docker/classroom/dev_run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+ROOT_DIR=/data/issdl
+HOST_HOME_DIR=$ROOT_DIR/home
+CORE_DIR=$ROOT_DIR/core
+
+mkdir -p $HOST_HOME_DIR $CORE_DIR
+
+cp ~/students.txt $CORE_DIR/
+cp ~/speakers.txt $CORE_DIR/
+cp ~/speaker_lectures_map.txt $CORE_DIR/
+
+docker run --hostname=issdl_classroom_dev -v $CORE_DIR:/core/ -v $HOST_HOME_DIR:/home/ -p 7000:7000 -ti issdl/classroom:2019-dev $@
+
